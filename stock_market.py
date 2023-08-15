14c97d56270d2ec46c91980298f84169d49da52c
Nithyaravi88 <90675842+Nithyaravi88@users.noreply.github.com>
2023-08-15 13:40:21 +0530

    Add files via upload

--- stock_market.py
+++ stock_market.py
@@ -2,7 +2,28 @@ import time
 import math
 
 class Stock:
+    """
+    A class representing a stock.
+    
+    Attributes:
+        stock_symbol (str): The symbol of the stock.
+        stock_type (str): The type of the stock (Common or Preferred).
+        last_dividend (float): The last dividend of the stock.
+        fixed_dividend (float): The fixed dividend (for Preferred stocks).
+        par_value (float): The par value of the stock.
+        trades (list): List of trade records for the stock.
+    """
     def __init__(self, stock_symbol, stock_type, last_dividend, fixed_dividend=None, par_value=0):
+        """
+        Initialize a Stock instance.
+        
+        Args:
+            stock_symbol (str): The symbol of the stock.
+            stock_type (str): The type of the stock (Common or Preferred).
+            last_dividend (float): The last dividend of the stock.
+            fixed_dividend (float, optional): The fixed dividend (for Preferred stocks). Default is None.
+            par_value (float, optional): The par value of the stock. Default is 100.
+        """
         self.symbol = stock_symbol
         self.stock_type = stock_type
         self.last_dividend = last_dividend
@@ -12,6 +33,15 @@ class Stock:
 
     #Calculate dividend yield
     def calculate_dividend_yield(self, price):
+        """
+        Calculate the dividend yield for the stock.
+
+        Args:
+            price (float): The current price of the stock.
+
+        Returns:
+            float: The dividend yield.
+        """
         if price <= 0:
             return None
         if self.stock_type == "Common":
@@ -23,6 +53,15 @@ class Stock:
 
     #Calculate PE ratio
     def calculate_pe_ratio(self, price):
+        """
+        Calculate the P/E ratio for the stock.
+
+        Args:
+            price (float): The current price of the stock.
+
+        Returns:
+            float: The P/E ratio.
+        """
         if price <= 0:
             return None
         if self.last_dividend == 0:
@@ -30,14 +69,28 @@ class Stock:
         return round(price / self.last_dividend ,2)
 
     #Recording trade
-    def record_trade(self, quantity, is_buy, price):
+    def record_trade(self, quantity, indicator, price):
+        """
+        Record a trade for the stock.
+
+        Args:
+            quantity (int): The quantity of shares traded.
+            trade_type (str): 'buy' for buy trade, 'sell' for sell trade.
+            price (float): The traded price of the shares.
+        """
         timestamp = time.time()
-        trade = {'timestamp': timestamp, 'quantity': quantity, 'buy': is_buy, 'price': price}
+        trade = {'timestamp': timestamp, 'quantity': quantity, 'indicator': indicator, 'price': price}
         print("Record a trade ", trade)
         self.trades.append(trade)
 
     #calculate Volume weighted Stock Price
     def calculate_volume_weighted_stock_price(self):
+        """
+        Calculate the volume-weighted stock price for the stock.
+
+        Returns:
+            float: The volume-weighted stock price.
+        """
         now = time.time()
         total_price_quantity = 0
         total_quantity = 0
@@ -51,6 +104,15 @@ class Stock:
 
 #calculate geometric mean
 def calculate_geometric_mean(prices):
+    """
+    Calculate the geometric mean of a list of prices.
+
+    Args:
+        prices (list): List of prices for which to calculate the geometric mean.
+
+    Returns:
+        float: The geometric mean of prices.
+    """
     if len(prices) == 0:
         return None
     product = 1
